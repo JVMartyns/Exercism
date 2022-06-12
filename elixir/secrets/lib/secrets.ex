b8aad9ad2f2691523e8b30391d275101a3cd0590
JVMartyns <jvmartyns2015@gmail.com>
2022-06-12 17:38:35 -0300

    solved

--- elixir/secrets/lib/secrets.ex
+++ elixir/secrets/lib/secrets.ex
@@ -1,29 +1,21 @@
 defmodule Secrets do
-  def secret_add(secret) do
-    # Please implement the secret_add/1 function
-  end
+  def secret_add(secret), do: &(&1 + secret)
 
-  def secret_subtract(secret) do
-    # Please implement the secret_subtract/1 function
-  end
+  def secret_subtract(secret), do: &(&1 - secret)
 
-  def secret_multiply(secret) do
-    # Please implement the secret_multiply/1 function
-  end
+  def secret_multiply(secret), do: &(&1 * secret)
 
-  def secret_divide(secret) do
-    # Please implement the secret_divide/1 function
-  end
+  def secret_divide(secret), do: &div(&1, secret)
 
-  def secret_and(secret) do
-    # Please implement the secret_and/1 function
-  end
+  def secret_and(secret), do: &Bitwise.band(secret, &1)
 
-  def secret_xor(secret) do
-    # Please implement the secret_xor/1 function
-  end
+  def secret_xor(secret), do: &Bitwise.bxor(secret, &1)
 
   def secret_combine(secret_function1, secret_function2) do
-    # Please implement the secret_combine/2 function
+    fn x ->
+      x
+      |> secret_function1.()
+      |> secret_function2.()
+    end
   end
 end
