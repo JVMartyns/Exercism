6692cf64bc5b211b62c9c28b8eb5976829bdaf92
JVMartyns <jvmartyns2015@gmail.com>
2022-06-13 09:04:50 -0300

    solved

--- elixir/log-level/lib/log_level.ex
+++ elixir/log-level/lib/log_level.ex
@@ -1,9 +1,24 @@
 defmodule LogLevel do
   def to_label(level, legacy?) do
-    # Please implement the to_label/2 function
+    cond do
+      level == 0 and legacy? == false -> :trace
+      level == 1 -> :debug
+      level == 2 -> :info
+      level == 3 -> :warning
+      level == 4 -> :error
+      level == 5 and legacy? == false -> :fatal
+      :else -> :unknown
+    end
   end
 
   def alert_recipient(level, legacy?) do
-    # Please implement the alert_recipient/2 function
+    label = to_label(level, legacy?)
+
+    cond do
+      label == :error or label == :fatal -> :ops
+      label == :unknown and legacy? == true -> :dev1
+      label == :unknown and legacy? == false -> :dev2
+      :else -> false
+    end
   end
 end
